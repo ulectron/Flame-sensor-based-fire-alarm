54c0d8d1d74f57d14c011c214aced08802b59cab
ulectron <ulectron@gmail.com>
2020-01-24 10:27:11 +0530

    Add files via upload

---
+++ flame_sensor.ino
@@ -0,0 +1,28 @@
+const int buzzerPin = 13;
+const int flamePin = 11;
+int Flame = HIGH;
+
+void setup() 
+{
+  
+  pinMode(buzzerPin, OUTPUT);
+  pinMode(flamePin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() 
+{
+  Flame = digitalRead(flamePin);
+  if (Flame== LOW)
+  {
+    Serial.println("Fire!!!");
+    digitalWrite(buzzerPin, HIGH);
+    delay(500);
+  }
+  else
+  {
+    Serial.println("No worries");
+    digitalWrite(buzzerPin, LOW);
+    delay(500);
+  }
+}
